897d1c93a5e74f39f71d5cccd323debb8dd083c1
Max Presman <max@pubnub.com>
2016-01-22 14:47:20 -0800

    ecma6 the test

--- test/release/release.test.js
+++ test/release/release.test.js
@@ -1,37 +1,36 @@
-var assert = require('assert');
-var fs = require('fs');
-var path = require('path');
+/* global describe, it, __dirname */
 
-var packageJSON = require('../../package.json');
-var bowerJSON = require('../../bower.json');
-var versionFile = fs.readFileSync(path.resolve(__dirname, '../../VERSION'), 'UTF-8');
-var readMe = fs.readFileSync(path.resolve(__dirname, '../../README.md'), 'UTF-8');
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
 
-describe('release should be consistent', function () {
-  it('with a matching bower and npm module', function () {
+const packageJSON = require('../../package.json');
+const bowerJSON = require('../../bower.json');
+const versionFile = fs.readFileSync(path.resolve(__dirname, '../../VERSION'), 'UTF-8');
+const readMe = fs.readFileSync(path.resolve(__dirname, '../../README.md'), 'UTF-8');
+
+describe('release should be consistent', () => {
+  it('with a matching bower and npm module', () => {
     assert.equal(packageJSON.version, bowerJSON.version);
   });
 
-  it('with have a matching VERSION', function () {
+  it('with have a matching VERSION', () => {
     assert.equal(versionFile, bowerJSON.version);
   });
 
-  it('with bower valid entry point', function () {
+  it('with bower valid entry point', () => {
     assert.equal(bowerJSON.main, 'dist/pubnub-angular-' + bowerJSON.version + '.min.js');
   });
 
-  it('with packaged dist files', function () {
-    var fileList = fs.readdirSync(path.resolve(__dirname, '../../dist'));
+  it('with packaged dist files', () => {
+    const fileList = fs.readdirSync(path.resolve(__dirname, '../../dist'));
     assert.deepEqual(fileList, ['pubnub-angular-' + versionFile + '.js',
       'pubnub-angular-' + versionFile + '.min.js',
       'pubnub-angular-' + versionFile + '.min.js.map']);
   });
 
-  it('with updated readme', function (){
-    assert(readMe.indexOf('http://cdn.pubnub.com/sdk/pubnub-angular/pubnub-angular-' + versionFile + '.js') > 1);
-    assert(readMe.indexOf('https://cdn.pubnub.com/sdk/pubnub-angular/pubnub-angular-' + versionFile + '.js') > 1);
-    assert(readMe.indexOf('http://cdn.pubnub.com/sdk/pubnub-angular/pubnub-angular-' + versionFile + '.min.js') > 1);
-    assert(readMe.indexOf('https://cdn.pubnub.com/sdk/pubnub-angular/pubnub-angular-' + versionFile + '.min.js') > 1);
+  it('with updated readme', () => {
+    assert(readMe.indexOf('http(s)://cdn.pubnub.com/sdk/pubnub-angular/pubnub-angular-' + versionFile + '.js') > 1);
+    assert(readMe.indexOf('http(s)://cdn.pubnub.com/sdk/pubnub-angular/pubnub-angular-' + versionFile + '.min.js') > 1);
   });
-
 });
