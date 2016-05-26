08d158173f899a6fc2613403d4f5412fe9f1f84a
Martin Lagrange <lagrangemartin@gmail.com>
2016-05-26 14:56:07 -0700

    fix($pubnubChannel.$extend) Avoid to share extension accross every channel objects

--- test/unit/PubnubChannel.test.js
+++ test/unit/PubnubChannel.test.js
@@ -309,6 +309,22 @@ describe('$pubnubChannel', function () {
         var chan = ExtendedChannel('myChannel')
         expect(chan.bar).to.be.an('function');
       });
+      
+      it('shouldnt extend prototype of others pubnubChannels', function() {
+        
+        var bar = function(){ return 'bar'; }
+        var ExtendedChannel = $pubnubChannel.$extend({bar: bar});
+        
+        var extendedChannel = new ExtendedChannel('myChannel')
+        var nonExtendedChannel = $pubnubChannel('chan');
+        
+        expect(extendedChannel.bar).to.be.a('function');
+        expect(extendedChannel.bar()).to.be.equal('bar');
+          
+        expect(nonExtendedChannel.bar).to.not.be.a('function');
+        
+      });
+      
     });
   });  
   
