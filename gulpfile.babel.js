d5e807247a1d4525e86c10b36ba6bb816258ec3a
Max Presman <max@pubnub.com>
2016-10-06 14:57:12 -0700

    suuport gzipping of artifacts

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,13 +10,15 @@ import gulpUglify from 'gulp-uglify';
 import gulpRename from 'gulp-rename';
 import gulpMocha from 'gulp-mocha';
 import runSequence from 'run-sequence';
+import gzip from 'gulp-gzip';
+import packageJSON from './package.json';
 
 import karma from 'karma';
 
 import webpackConfig from './webpack.config';
 
 gulp.task('clean', function () {
-  return gulp.src(['dist'], { read: false }).pipe(gulpClean());
+  return gulp.src(['dist', 'upload'], { read: false }).pipe(gulpClean());
 });
 
 gulp.task('lint', function () {
@@ -64,6 +66,24 @@ gulp.task('test_client-pubnub-v3', function (done) {
     .start();
 });
 
+gulp.task('create_version', () => {
+  return gulp.src('dist/pubnub-angular.js')
+    .pipe(gulpRename('pubnub-angular-' + packageJSON.version + '.js'))
+    .pipe(gulp.dest('upload/normal'));
+});
+
+gulp.task('create_version_min', function () {
+  return gulp.src('dist/pubnub-angular.min.js')
+    .pipe(gulpRename('pubnub-angular-' + packageJSON.version + 'min.js'))
+    .pipe(gulp.dest('upload/normal'));
+});
+
+gulp.task('create_version_gzip', function () {
+  return gulp.src('upload/normal/*.js')
+    .pipe(gzip({ append: false }))
+    .pipe(gulp.dest('upload/gzip'));
+});
+
 gulp.task('test_client-pubnub-v4', function (done) {
   new karma.Server({ configFile: __dirname + '/karma.conf.pubnub-v4.js' }, done)
     .start();
@@ -73,8 +93,12 @@ gulp.task('test_client', function (done) {
   runSequence('test_client-pubnub-v3', 'test_client-pubnub-v4', done);
 });
 
+gulp.task('prepare_upload', function (done) {
+  runSequence('create_version', 'create_version_min', 'create_version_gzip', done);
+})
+
 gulp.task('compile', function (done) {
-  runSequence('clean', 'webpack', 'lint', 'uglify', 'include-sourcemaps', 'clean-original-sourcemap', done);
+  runSequence('clean', 'webpack', 'lint', 'uglify', 'include-sourcemaps', 'clean-original-sourcemap', 'prepare_upload', done);
 });
 
 gulp.task('test', (done) => {
